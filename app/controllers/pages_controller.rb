baddf9e66a4866bcbe20ce62bb48a1e3a0e695db
Larrire <linekerlarrire@gmail.com>
2021-07-22 13:57:49 -0300

    Adicionando bootstrap e alterando layout do blog

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+    def welcome
+        render "pages/welcome"
+    end
+
+end
---
+++ public/js/pages/welcome.js
@@ -0,0 +1,19 @@
+navbar = document.querySelector('.navbar');
+navbar.classList.add('onTop');
+logo = document.querySelector('#navbar-logo');
+logo.setAttribute('src', `${base_url}/blog-logo-white.png`)
+btnSearch = document.querySelector('#btnSearch');
+btnSearch.classList.remove('btn-outline-dark')
+
+window.onscroll = function(){
+    console.log(window.scrollY)
+    if( window.scrollY === 0 ){
+        navbar.classList.add('onTop');
+        logo.setAttribute('src', `${base_url}/blog-logo-white.png`)
+        btnSearch.classList.remove('btn-outline-dark')
+    } else {
+        navbar.classList.remove('onTop');
+        logo.setAttribute('src', `${base_url}/blog-logo-black.png`)
+        btnSearch.classList.add('btn-outline-dark')
+    }
+}
